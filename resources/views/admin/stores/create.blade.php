a4020c278a2144c14979a82fd1d9348205bc8ca0
Ellen Dourado <ellend.geral@gmail.com>
2020-05-27 14:38:35 -0300

    8:67/68 - Redirecionamento pós login e criando loja para user logado

--- resources/views/admin/stores/create.blade.php
+++ resources/views/admin/stores/create.blade.php
@@ -28,15 +28,6 @@
             <label>Slug</label>
             <input type="text" name="slug" class="form-control">
         </div>
-
-        <div class="form-group">
-            <label>Usuário</label>
-            <select name="user" id="" class="form-control">
-                @foreach($users as $user)
-                <option value="{{$user->id}}">{{$user->name}}</option>
-                @endforeach
-            </select>
-        </div>
         <div>
             <button type="submit" class="btn btn-success">Criar Loja</button>
         </div>
