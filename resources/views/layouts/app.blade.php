cd4ce90a58ae038e20bb48efcfeddbc96b5804e6
Ellen Dourado <ellend.geral@gmail.com>
2020-05-27 14:52:01 -0300

    8:69 - Conclusões módulo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,11 +25,15 @@
                 </ul>
                 <div class="my-2 my-lg-0">
                     <ul class="navbar-nav mr-auto">
-                        <li class="nav-item"></li>
-                        <a class="nav-link" onclick="event.preventDefault(); document.querySelector('form.logout').submit();">Sair</a>
-                        <form action="{{route('logout')}}" class="logout" method="POST" style="display: none;">
-                            @csrf
-                        </form>
+                        <li class="nav-item">
+                            <a class="nav-link" onclick="event.preventDefault(); document.querySelector('form.logout').submit();">Sair</a>
+                            <form action="{{route('logout')}}" class="logout" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                        </li>
+                        <li class="nav-item">
+                            <span class="nav-link">{{auth()->user()->name}}</span>
+                        </li>
                     </ul>
                 </div>
             </div>
